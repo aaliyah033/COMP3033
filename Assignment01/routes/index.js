120a61d9e0c6fcd7c18201a1199bffdd7a8eb29c
aaliyah033 <79179237+aaliyah033@users.noreply.github.com>
2025-10-14 21:53:59 -0400

    Added title and paragraph

--- Assignment01/routes/index.js
+++ Assignment01/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Project Assignment 1 – Web Frameworks and APIs' });
 });
 
 module.exports = router;
