d119a9e7a2f2c5d75b544aa8a9a12f35767cff5d
aaliyah033 <aaliyahali0727@gmail.com>
2025-12-03 13:35:28 -0500

    Added Helmet
    Additional feature 1

--- Assignment02/autospareparts/app.js
+++ Assignment02/autospareparts/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//ADDED - helmet for security
+var helmet = require('helmet');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -28,6 +31,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//ADDED - use helmet
+app.use(helmet());
 
 app.use(logger('dev'));
 app.use(express.json());
